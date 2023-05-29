3b167d882963615704e5c0af0445a3deb348c0d7
Katherine <angiecruzo.o@gmail.com>
2023-05-29 15:42:30 -0500

    areas de cuadrados y triangulos

--- scriptmath.js
+++ scriptmath.js
@@ -1,9 +1,57 @@
-const numero1 = 5;
-let numero2 = "3";
+/*Perimetros y diametros
+perimetro de un cuadrado: lado * 4 */
 
-function sumar(num1, num2){
-    console.log (num1 + num2);
-    return;
+console.group('cuadrado')
+
+
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log({
+    ladoCuadrado,
+    perimetroCuadrado,
+    areaCuadrado,
+});
+
+function calcularCuadrado(lado){
+    return {
+        perimetro: lado * 4,
+        area: lado * lado,
+    }
+}
+console.groupEnd('cuadrado');
+
+console.group('triangulo');
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3;
+
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+function calcularTriangulo(lado1, lado2, base, altura){
+    return {
+        perimetro: lado1 + lado2 + base,
+        area: (base * altura) / 2,
+    }
 }
 
-sumar(numero1 , numero2);
+console.log({
+ladoTriangulo1,
+ladoTriangulo2,
+ladoTrianguloBase,
+perimetroTriangulo,
+alturaTriangulo,
+});
+
+console.groupEnd('triangulo');
+
+/*area de un cuadrado: lado * lado
+perimetro de un triangulo: lado + lado + lado
+area del triangulo: (base * altura) / 2
+perimetro de un circulo: radio al cuadrado * pi(3.1415)*/
