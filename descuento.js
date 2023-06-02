f81fff877633e1e5b05b48097719d27f565fd9e9
Katherine <angiecruzo.o@gmail.com>
2023-06-01 20:26:41 -0500

    calculo de descuentos a partir de cupones

---
+++ descuento.js
@@ -0,0 +1,34 @@
+const precioInicialInput = document.querySelector('.costo-inicial');
+const cuponPorcentajeInput = document.querySelector('.cupon-ingresado');
+const botonCalculo = document.querySelector('.boton');
+const pResult = document.querySelector('#result')
+const coupons = {
+    diaMadres: 50,
+    valentin: 20,
+    christmas: 30,
+    blackFriday: 55,
+}
+
+botonCalculo.addEventListener('click', calcularPrecioFinal);
+
+function calcularPrecioFinal(event){
+    event.preventDefault();
+    const cuponPorcentaje = coupons[cuponPorcentajeInput.value];
+    console.log(cuponPorcentaje);
+    const precioInicial = precioInicialInput.value
+   
+    console.log('prueba');
+
+    if(!precioInicial || !cuponPorcentajeInput.value){
+        pResult.innerText = "Ingresa los datos que se requieren para calcular el costo final que deseas"
+        return;
+    }
+    if(!cuponPorcentaje){
+        pResult.innerText = "el cupon que ingresaste es invalido, vuelve a ingresar el nombre del cupon (tener presente mayusculas y minusculas)"
+        return;
+    }
+    
+    const resta = (100 - cuponPorcentaje)
+    const newPrice = (precioInicial * resta) / 100;
+    pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
+};
--- tiendajs.js
+++ tiendajs.js
@@ -1,10 +1,9 @@
 /* formula para porcentajes (P * (100 - D) / 100 */
-
 const precioInicialInput = document.querySelector('.costo-inicial');
 const descuentoPorcentaje = document.querySelector('.descuento');
 const botonCalculo = document.querySelector('.boton');
 const ValorFinal = document.querySelector('.costo-final');
-
+const pResult = document.querySelector('#result')
 
 botonCalculo.addEventListener('click', calcularPrecioFinal);
 
@@ -12,8 +11,16 @@ function calcularPrecioFinal(event){
     event.preventDefault();
     const precioInicial = precioInicialInput.value
     const descuento = descuentoPorcentaje.value
+
+    if(!precioInicial || !descuentoPorcentaje){
+        pResult.innerText = "Ingresa los datos que se requieren para calcular el costo final que deseas"
+        return;
+    }
+    if(descuento>100){
+        pResult.innerText = "el porcentaje de descuento no corresponde"
+        return;
+    }
     const resta = 100 - descuento
-
     ValorFinal.value = (precioInicial * resta) / 100;
+    pResult.innerText = "";
 };
-
