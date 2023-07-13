f3a35f9478e83309cc4417c07a5f8eab915e5b19
Katherine <angiecruzo.o@gmail.com>
2023-07-13 12:58:38 -0500

    inicio analisis empresarial

--- analisis.js
+++ analisis.js
@@ -2,9 +2,10 @@ console.log(salarios);
 
 // realizar el analiss personal de juanita
 
-
 function encontrarPersona(personaEnBusqueda){
-    return salarios.find(persona => persona.name == personaEnBusqueda);
+    return salarios.find(function(persona){
+     return persona.name === personaEnBusqueda
+    })
 };
 
 function medianaPorPersona(nombrePersona) {
@@ -42,3 +43,48 @@ function proyeccionPorPersona(nombrePersona) {
   
     return nuevoSalario;
   }
+
+/* Analisis empresarial
+un objeto que por dentro tenga distintas empresas donde dentro tendran otro obejto con los
+years 2018, 2019, 2020 y dentro tendra un array con los salarios de cada year
+industrias mokepon:{
+    2018: [salario, salarios, salario]
+    2019: [salario, salarios, salario]},
+*/
+
+// POSIBLE INICIO DE SOLUCION
+// function empresas(arraySalarios){
+// let Empresa = {}
+
+// for(let i=0; i < arraySalarios.length; i++){
+//     for(let j=0; j < arraySalarios[i].trabajos.length; j++){
+//         console.log('i: ', i, 'j: ', j, 'salarios:' , salarios[i].trabajos[j])
+//     }
+// }
+const empresas = {};
+
+for (persona of salarios) {
+  for (trabajo of persona.trabajos) {
+    if (!empresas[trabajo.empresa]) {
+      empresas[trabajo.empresa] = {};
+    }
+
+    if (!empresas[trabajo.empresa][trabajo.year]) {
+      empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+  }
+}
+
+console.log({empresas});
+
+function medianaEmpresaYear(nombre, year) {
+  if (!empresas[nombre]) {
+    console.warn('La empresa no existe');
+  } else if (!empresas[nombre][year]) {
+    console.warn('La empresa no dio salarios ese año');
+  } else {
+    return PlatziMath.calcularMediana(empresas[nombre][year]);
+  }
+}
